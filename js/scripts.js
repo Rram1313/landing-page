901855d313dd2abb10e64a6ae445d7bc309cc6a5
Rram1313 <141140169+Rram1313@users.noreply.github.com>
2024-04-18 12:33:26 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,10 @@ function toggleMusic() {
   }
 }
 
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+});
